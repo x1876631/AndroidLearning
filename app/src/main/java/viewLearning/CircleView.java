7b85e79219f3915ce6e84aeb5f978a57c9c46969
xuye <1554922833@qq.com>
2016-11-19 22:12:10 +0800

    完善自定义绘制圆，增加对padding的设置

--- app/src/main/java/viewLearning/CircleView.java
+++ app/src/main/java/viewLearning/CircleView.java
@@ -10,6 +10,11 @@ import android.view.View;
 /**
  * Created by xuye on 16/11/18
  * 自定义view学习，只是简单的绘制一个圆
+ * <p/>
+ * 【注意】
+ * 1、直接继承view，需要对padding做处理，否则padding会失效
+ * 2、直接继承view，需要对wrap_content做处理，否则wrap_content效果等同于match_parent
+ * 为什么会导致上面的问题呢？看源码
  */
 public class CircleView extends View {
 
@@ -38,9 +43,16 @@ public class CircleView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int width = getWidth();
-        int height = getHeight();
+        int paddingLeft = getPaddingLeft();
+        int paddingRight = getPaddingRight();
+        int paddingTop = getPaddingTop();
+        int paddingBottom = getPaddingBottom();
+        //获取圆的宽高，例如：圆的宽度 = view的宽度 - view的左右padding
+        int width = getWidth() - paddingLeft - paddingRight;
+        int height = getHeight() - paddingTop - paddingBottom;
+        //获取要绘制的圆的半径
         int radius = Math.min(width, height) / 2;
-        canvas.drawCircle(width / 2, height / 2, radius, mPaint);
+        //绘制圆，padding作用于圆上
+        canvas.drawCircle(paddingLeft + width / 2, paddingTop + height / 2, radius, mPaint);
     }
 }
