6b854a581aa523744b9504634979dc99d68ac511
xuye <1554922833@qq.com>
2017-10-24 22:59:35 +0800

    测试application弹toast

--- app/src/main/java/com/xuye/androidlearning/other/OtherLearingActivity.java
+++ app/src/main/java/com/xuye/androidlearning/other/OtherLearingActivity.java
@@ -1,6 +1,8 @@
 package com.xuye.androidlearning.other;
 
 import android.os.Bundle;
+import android.os.Looper;
+import android.widget.Toast;
 
 import com.xuye.androidlearning.R;
 import com.xuye.androidlearning.base.CommonTestActivity;
@@ -18,13 +20,34 @@ public class OtherLearingActivity extends CommonTestActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         showItemWithCount(new String[]{
-                getString(R.string.show_phone_tel)
+                getString(R.string.show_phone_tel), "非UI线程弹toast", "内存泄露测试"
         });
-        MemoryLeakTestHelper.getInstance().log(this);
+
+
+        Toast.makeText(getApplicationContext(), "UI线程，测试application弹toast", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void clickButton1() {
         DeviceUtils.getPhoneTelNumber(OtherLearingActivity.this);
+    }
+
+    @Override
+    protected void clickButton2() {
+        super.clickButton2();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                Toast.makeText(getApplicationContext(), "非UI线程，测试application弹toast", Toast.LENGTH_SHORT).show();
+                Looper.loop();
+            }
+        }).start();
+    }
+
+    @Override
+    protected void clickButton3() {
+        super.clickButton3();
+        MemoryLeakTestHelper.getInstance().log(this);
     }
 }
